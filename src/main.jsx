d886bdca966b9c42fe8c5fdb00c34a3ee70fcf8a
Bank <bankboupha@gmail.com>
2024-11-17 20:06:58 +0700

    basename added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router/WebRouter";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <BrowserRouter basename="/Login-demo/">
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>
 )
